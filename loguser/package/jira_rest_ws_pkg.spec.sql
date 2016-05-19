58f6c042225e780d39a9b993436955c361526d43
GITHUB <user.GITHUB@orcldocs.db>
2016-05-19 14:32:15 +0300

    Auto push from github_utl

--- loguser/package/jira_rest_ws_pkg.spec.sql
+++ loguser/package/jira_rest_ws_pkg.spec.sql
@@ -1,8 +1,12 @@
 
   CREATE OR REPLACE EDITIONABLE PACKAGE "LOGUSER"."JIRA_REST_WS_PKG" IS
 
--- Author  : ASLUCHKO
--- Created : 19.05.2016 13:08:16
--- Purpose : 
+  -- Author  : ASLUCHKO
+  -- Created : 19.05.2016 13:08:16
+  -- Purpose : 
+
+  FUNCTION get_issue(in_jira_base_url  IN VARCHAR2,
+                     in_jira_issue_id  IN NUMBER,
+                     in_jira_issue_key IN VARCHAR2) RETURN t_jira_issue;
 
 END jira_rest_ws_pkg;
